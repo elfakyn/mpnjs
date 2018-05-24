139288ca6f1e904f821ccee7e0f7146e512402a4
Arya Popescu <arya.popescu@mapbox.com>
2018-05-24 15:38:40 -0400

    initial commit

---
+++ static.npmjs.com/flatpage.6bb46a2b7cd19e797e58.js
@@ -0,0 +1,2 @@
+webpackJsonp([46],{488:function(a,b,c){const e=c(2);var f=c(0);a.exports=function(h){var i=h.flatpage;return f.createElement('div',{id:i.ref,dangerouslySetInnerHTML:{__html:i.data}})};let g;e.register('flatpage',a.exports,g)}},[488]);
+//# sourceMappingURL=flatpage.6bb46a2b7cd19e797e58.js.map
